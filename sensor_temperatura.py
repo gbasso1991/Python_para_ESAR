fa23f04696d312d8ba53ad1d3fff5c7eb10f7ced
gbasso1991 <giulianoandresbasso@gmail.com>
2022-04-26 00:20:50 -0300

    update

--- sensor_temperatura.py
+++ sensor_temperatura.py
@@ -1,39 +1,49 @@
 #%%sensor_temperatura.py
-
-from msilib.schema import ServiceInstall
+from tkinter import *
 import serial.tools.list_ports
+import functools
 
 ports = serial.tools.list_ports.comports()
+serialObj = serial.Serial()
 
-serialInst = serial.Serial()  # Instancia vacia del objeto Serial
-#%%#para leer armo loop
-portList =[]
+
+root = Tk()
+root.config(bg='grey')
+
+def initComPort(index):
+    currentPort=str(ports[index])
+    #print(currentPort)
+    comPortVar= str(currentPort.split(' ')[0])
+    serialObj.port= comPortVar
+    serialObj.baudrate= 9600
+    serialObj.open()
 
 for onePort in ports:
-    portList.append(str(onePort))
-    print(str(onePort))
+    comBotton= Button(root,text=onePort,font=('Calibri','13'),height=1,width=45 )
+    comBotton.grid(row=ports.index(onePort),column=0,command= functools.partial(initComPort,index=ports.index(onePort)))
+
+dataCanvas = Canvas(root,width=600,height=400,bg='white')
+dataCanvas.grid(row=0,column=1,rowspan=100)
+
+vsb = Scrollbar(root,orient='vertical',command= dataCanvas.yview)
+vsb.grid(row=0,column=2,rowspan=100,sticky='ns')
+
+dataCanvas.config(yscrollcommand=vsb.set)
+
+dataFrame= Frame(dataCanvas,bg='white')
+dataCanvas.create_window((10,0),window=dataFrame,anchor='nw')
 
 
-val = input('Seleccionar puerto: COM')
-print(val)
+def checkSerialPort():
+    if serialObj.isOpen() and serialObj.in_waiting:
+        recentPacket = serialObj.readline()
+        recentPacketString = recentPacket.decode('utf').rstrip('\n')
+        Label(dataFrame,text=recentPacketString,font=('Calibri','13'),bg='white').pack() 
 
-for x in range(len(portList)):
-    if portList[x].startswith('COM'+str(val)):
-        portVar='COM'+str(val)
-        print(portList[x])
-#%%
-serialInst.baudrate = 9600
-serialInst.port = portVar
-serialInst.open() 
+
 
 while True:
-    if serialInst.in_waiting:
-        packet = serialInst.readline()
-        print(packet.decode('bit'))
+    root.update()
+    checkSerialPort()
+    dataCanvas.config(scrollregion=dataCanvas.bbox('all'))
 
-
-# %%
-import serial
-ser = serial.Serial()
-ser.baudrate = 9600
-ser.port = 'COM3'
